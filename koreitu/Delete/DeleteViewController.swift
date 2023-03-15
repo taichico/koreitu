63a626041434a1ab9dce85e974da952be9bc2a32
Taichi Yoshioka <yoshioka3noadress@git.com>
2023-03-15 16:32:39 +0900

    データ削除画面のUI作成

--- koreitu/Delete/DeleteViewController.swift
+++ koreitu/Delete/DeleteViewController.swift
@@ -3,9 +3,11 @@ import UIKit
 class DeleteViewController: UIViewController, DeleteViewProtocol {
     var presenter: DeletePresenter?
     
-
+    @IBOutlet weak var deleteTextFieild: UITextField!
+    @IBOutlet weak var deleteTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
     }
 }
